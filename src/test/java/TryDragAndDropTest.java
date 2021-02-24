ed435e53d89147f44589992f81b2913f57e8af51
AnnaPedych <apedych@gmail.com>
2021-02-24 10:55:17 +0200

    fixed

--- src/test/java/TryDragAndDropTest.java
+++ src/test/java/TryDragAndDropTest.java
@@ -2,6 +2,9 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static com.codeborne.selenide.Condition.text;
@@ -14,21 +17,11 @@ public class TryDragAndDropTest {
     }
     @Test
     void PositiveDragAndDropTest() {
+        
         open("https://the-internet.herokuapp.com/drag_and_drop");
         SelenideElement element1 = $("#column-a");
         SelenideElement element2 = $("#column-b");
-
-        //actions().dragAndDrop(element1, element2).build().perform();
-
-        //actions().clickAndHold(element).build().perform();
-        //actions().moveToElement($("#column-b")).release($("#column-b")).build().perform();
-
-        //$("#column-a").dragAndDropTo("#column-b");
-
-        //actions().moveToElement(element1).clickAndHold().moveByOffset(300,200).release().perform();
-
-        //actions().clickAndHold(element1).moveByOffset(300,200).release().perform();
-
+        $("#column-a").dragAndDropTo("#column-b");
         $(element1).shouldHave(text("B"));
         $(element2).shouldHave(text("A"));
     }
